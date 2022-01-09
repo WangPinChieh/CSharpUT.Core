79208b8cc75a7a961bcf0a42c6f366abd8044a64
Jay Wang <jay.wang@hp.com>
2022-01-09 14:45:44 +0800

    fix:auth tests

--- HolidayTests/AuthenticationServiceTests.cs
+++ HolidayTests/AuthenticationServiceTests.cs
@@ -9,12 +9,35 @@ namespace HolidayTests
         [Test()]
         public void is_valid()
         {
-            var target = new AuthenticationService();
+            var profile = new MockProfile();
+            var token = new MockToken();
+            var target = new AuthenticationService(profile, token);
 
             var actual = target.IsValid("joey", "91000000");
 
             //always failed
             Assert.IsTrue(actual);
+        }
+    }
+
+    public class MockToken : IToken
+    {
+        public string GetRandom(string account)
+        {
+            return "000000";
+        }
+    }
+
+    public class MockProfile : IProfile
+    {
+        public string GetPassword(string account)
+        {
+            if (account == "joey")
+            {
+                return "91";
+            }
+
+            return "";
         }
     }
 }
