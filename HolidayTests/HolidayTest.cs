2453ff3c166d87a1908ca7e77bf535d5fa49c4af
Jay Wang <jay.wang@hp.com>
2022-01-09 11:14:02 +0800

    refactor: HolidayTest

--- HolidayTests/HolidayTest.cs
+++ HolidayTests/HolidayTest.cs
@@ -7,31 +7,36 @@ namespace HolidayTests
     [TestFixture]
     public class HolidayTest
     {
-        private readonly FakeHoliday _holiday = new FakeHoliday();
+        private FakeHoliday _holiday;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _holiday = new FakeHoliday();
+        }
 
         [Test]
         public void today_is_not_xmas()
         {
-            GivenToday(new DateTime(2022, 1, 9));
+            GivenToday(1, 9);
             ShouldReturn("Today is not Xmas");
         }
-
-
 
         [Test]
         public void today_is_xmas()
         {
-            GivenToday(new DateTime(2022, 12, 25));
+            GivenToday(12, 25);
             ShouldReturn("Merry Xmas");
         }
 
-        private void GivenToday(DateTime date)
-        {
-            _holiday.Today = date;
-        }
         private void ShouldReturn(string expected)
         {
             Assert.AreEqual(expected, _holiday.SayHello());
+        }
+
+        private void GivenToday(int month, int day)
+        {
+            _holiday.Today = new DateTime(2022, month, day);
         }
     }
 
